7968acac1bed48b217c27b7ace554ccd3b9153f4
rudy3091 <grayblack313@gmail.com>
2021-05-06 00:08:52 +0900

    Initial commit

---
+++ src/test/java/com/caulipse/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caulipse.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
