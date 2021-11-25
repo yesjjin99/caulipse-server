09052cfd7e2f312d98ec44bbd47b67e743a3f416
rudy3091 <grayblack313@gmail.com>
2021-11-25 21:27:47 +0900

    Fix entity files
    엔티티 파일들 모두 src/entity 하위로 이동

--- src/entity/StudyEntity.ts
+++ src/entity/StudyEntity.ts
@@ -1,62 +1,7 @@
-import {
-  Entity,
-  BaseEntity,
-  Column,
-  PrimaryGeneratedColumn,
-  CreateDateColumn,
-  OneToMany,
-  ManyToOne,
-  JoinColumn,
-} from 'typeorm';
-import User from './UserEntity';
-import StudyCategory from './StudyCategoryEntity';
+import { Entity, PrimaryGeneratedColumn } from 'typeorm';
 
-@Entity({ name: 'STUDY' })
-export default class Study extends BaseEntity {
-  @PrimaryGeneratedColumn('uuid', { name: 'STUDY_ID' })
+@Entity({ name: 'Study' })
+export default class Study {
+  @PrimaryGeneratedColumn('uuid', { name: 'ID' })
   id!: string;
-
-  @CreateDateColumn({ name: 'CREATED_AT' })
-  createdAt!: Date;
-
-  @Column({ name: 'TITLE' })
-  title!: string;
-
-  @Column({ name: 'STUDY_ABOUT' })
-  studyAbout!: string;
-
-  @Column('int', { name: 'TIME' })
-  time!: number;
-
-  @Column('int', { name: 'WEEKDAY' })
-  weekday!: number;
-
-  @Column('int', { name: 'FREQUENCY' })
-  frequency!: number;
-
-  @Column({ name: 'LOCATION' })
-  location!: string;
-
-  @ManyToOne((type) => User, (user) => user.id)
-  @JoinColumn({ name: 'HOST_ID' })
-  hostId!: User;
-
-  @Column('int', { name: 'CAPACITY' })
-  capacity!: number;
-
-  @Column('int', { name: 'MEMBERS_COUNT' })
-  membersCount!: number;
-
-  @Column('int', { name: 'VACANCY' })
-  vacancy!: number;
-
-  @Column({ name: 'IS_OPEN' })
-  isOpen!: boolean;
-
-  @OneToMany((type) => StudyCategory, (studycategory) => studycategory.studyId)
-  @JoinColumn({ name: 'CATEGORY' })
-  category!: StudyCategory[];
-
-  @Column('int', { name: 'VIEWS' })
-  views!: number;
 }
--- src/models/StudyEntity.ts
+++
@@ -1,7 +0,0 @@
-import { Entity, PrimaryGeneratedColumn } from 'typeorm';
-
-@Entity({ name: 'Study' })
-export default class Study {
-  @PrimaryGeneratedColumn('uuid', { name: 'ID' })
-  id!: string;
-}
