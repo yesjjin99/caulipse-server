bdc41943d872a2a23acdacf6233b661054772b6b
yesjjin99 <yesjjin99@naver.com>
2021-12-07 22:48:43 +0900

    Fix/Update Entities

--- src/entity/CategoryEntity.ts
+++ src/entity/CategoryEntity.ts
@@ -1,20 +1,15 @@
-import {
-  Entity,
-  BaseEntity,
-  PrimaryGeneratedColumn,
-  Column,
-  OneToMany,
-  JoinColumn,
-} from 'typeorm';
+import { Entity, BaseEntity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
 @Entity({ name: 'CATEGORY' })
 export default class Category extends BaseEntity {
   @PrimaryGeneratedColumn({ name: 'CODE' })
   code!: number;
 
-  @Column('enum', { name: 'MAIN' })
+  // enum 타입으로 수정 필요
+  @Column({ name: 'MAIN' })
   main!: string;
 
-  @Column('enum', { name: 'SUB' })
+  // enum 타입으로 수정 필요
+  @Column({ name: 'SUB' })
   sub!: string;
 }
