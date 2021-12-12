6bdcf6f8550854abda707775bfdbe3e441971694
rudy3091 <grayblack313@gmail.com>
2021-12-12 17:40:35 +0900

    Add profile router
    /api/user/profile 에 해당하는 요청을 처리할 라우터 추가

--- src/routes/user/index.ts
+++ src/routes/user/index.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 import idRouter from './id';
+import profileRouter from './profile';
 
 const router = Router();
+router.use('/profile', profileRouter);
 router.use('/:id', idRouter);
 
 export default router;
