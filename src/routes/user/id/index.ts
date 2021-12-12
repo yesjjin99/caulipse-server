fffbff85350fc521898be8245d6b0dcb4f183cd2
rudy3091 <grayblack313@gmail.com>
2021-12-12 17:47:15 +0900

    Add notification router
    /api/user/:userId/:notificaion/:notiId 에 해당하는 요청을 처리할 라우터
    추가

--- src/routes/user/id/index.ts
+++ src/routes/user/id/index.ts
@@ -1,5 +1,7 @@
 import { Router } from 'express';
+import notificationRouter from './notification';
 
 const router = Router();
+router.use('/notification', notificationRouter);
 
 export default router;
---
+++ src/routes/user/id/notification/index.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+import helloWorld from '../../../hello-world';
+
+const router = Router();
+// 사용자의 알림 확인 상태 갱신
+router.patch('/:notiId', helloWorld);
+// 사용자의 알림 항목을 삭제
+router.delete('/:notiId', helloWorld);
+
+export default router;
