898116ab3358cf1021e4736e77f37ed43ed0a690
rudy3091 <grayblack313@gmail.com>
2021-12-12 17:52:06 +0900

    Add user/category router
    /api/user/:userId/category 의 요청을 처리할 라우터 추가

--- src/routes/user/id/index.ts
+++ src/routes/user/id/index.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 import notificationRouter from './notification';
+import categoryRouter from './category';
 
 const router = Router();
 router.use('/notification', notificationRouter);
+router.use('/category', categoryRouter);
 
 export default router;
