385fc8cfab3761aaebb7987a9703f898ba406ca8
rudy3091 <grayblack313@gmail.com>
2021-12-17 13:40:24 +0900

    Add study/:id/bookmark router
    /api/study/:id/bookamrk 요청을 처리할 라우터 추가

--- src/routes/study/id/index.ts
+++ src/routes/study/id/index.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 import studyCommentRouter from './comment';
+import studyBookmarkRouter from './bookmark';
 
 const router = Router();
 router.use('/comment', studyCommentRouter);
+router.use('/bookmark', studyBookmarkRouter);
 
 export default router;
