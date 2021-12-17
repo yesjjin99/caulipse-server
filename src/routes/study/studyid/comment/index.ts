65061567151fe9cbc14c65f1194fc6cec5f8be0f
rudy3091 <grayblack313@gmail.com>
2021-12-17 14:38:40 +0900

    Add study/comment controller
    study/comment 태그 아래의 router들에 controller 등록

--- src/routes/study/studyid/comment/index.ts
+++ src/routes/study/studyid/comment/index.ts
@@ -1,5 +1,14 @@
 import { Router } from 'express';
+import helloWorld from '../../../hello-world';
 
 const router = Router();
+// 스터디 문의글 목록 조회
+router.get('/', helloWorld);
+// 새로운 스터디 분의글 등록
+router.post('/', helloWorld);
+// 스터디 문의글 수정
+router.patch('/:commentId', helloWorld);
+// 스터디 문의글 삭제
+router.delete('/:commentId', helloWorld);
 
 export default router;
