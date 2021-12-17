26620e69f5929c990312897d06d14292b0467ae5
rudy3091 <grayblack313@gmail.com>
2021-12-17 13:34:53 +0900

    Add study/id router
    /api/study/id 요청을 처리할 라우터 추가

--- src/routes/study/index.ts
+++ src/routes/study/index.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 import commentRouter from './comment';
+import idRouter from './id';
 
 const router = Router();
 router.use('/comment', commentRouter);
+router.use('/:id', idRouter);
 
 export default router;
