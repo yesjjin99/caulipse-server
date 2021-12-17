aa576866044fea3bff61f81710915dca66498f4e
rudy3091 <grayblack313@gmail.com>
2021-12-17 14:49:50 +0900

    Add bookmark controller
    bookmark 태그 아래의 router들에 controller 등록

---
+++ src/routes/user/id/bookmark/index.ts
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+import helloWorld from '../../../hello-world';
+
+const router = Router();
+// 사용자의 북마크 목록 조회
+router.get('/', helloWorld);
+
+export default router;
