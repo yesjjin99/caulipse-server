890b17e67330274126a88a79fea956250def087d
rudy3091 <grayblack313@gmail.com>
2021-12-17 13:27:57 +0900

    Add study router
    /api/study 요청을 처리할 study router 추가

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 import userRouter from './user';
+import studyRouter from './study';
 
 const router = Router();
 router.use('/api/user', userRouter);
+router.use('/api/study', studyRouter);
 
 export default router;
