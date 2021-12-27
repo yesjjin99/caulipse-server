ced585df2e7aacc62f620a17a8343bcd0de14373
rudy3091 <grayblack313@gmail.com>
2021-12-28 04:20:43 +0900

    Add user role changing controller funtion
    사용자의 권한을 수정하는 controller 함수 추가
    이메일을 통해 이동한 링크에서 해당 경로로 요청을 보내면 됨

--- src/services/User/index.ts
+++ src/services/User/index.ts
@@ -4,6 +4,7 @@ import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
 import User, { UserRoleEnum } from '../../entity/UserEntity';
 import { makeSignUpToken } from '../../utils/auth';
+import jwt from 'jsonwebtoken';
 
 /**
  * @swagger
@@ -52,5 +53,76 @@ export const saveUser = async (req: Request, res: Response) => {
     res.status(201).json({ message: '회원가입 성공', id });
   } catch (e) {
     res.status(400).json({ message: '회원가입 실패: ' + (e as Error).message });
+  }
+};
+
+/**
+ * @swagger
+ * /user/role/:userid:
+ *  patch:
+ *    tags:
+ *    - user
+ *    summary: 사용자 권한 수정(회원가입 절차 마무리)
+ *    description: 회원가입을 요청한 사용자가 이메일에서 링크를 클릭해 학교메일을 인증할 시 사용될 api
+ *    parameters:
+ *    - in: path
+ *      name: userid
+ *      description: 회원가입하는 사용자의 id (요청 경로의 '-'를 허용해야하나?)
+ *      required: true
+ *      example: 15f6d6ee-32e2-4036-b050-fa79e38dcd36
+ *    - in: body
+ *      name: body
+ *      schema:
+ *        type: object
+ *        properties:
+ *          token:
+ *            type: string
+ *            example: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjUxYjg0M2I5LWE5ZTEtNDk3Mi05NWNiLWVmYTcxYzY2ODI5NSIsImlhdCI6MTY0MDYzMDg2MiwiZXhwIjoxNjQwNzE3MjYyfQ.zLJtNN-VCuYlghtE2v0yDJbz7YuxedGHKLt6CW7tUnA
+ *            description: 사용자를 인증할 jwt토큰, email의 링크에 쿼리스트링으로 보내줄 예정
+ */
+export const changeUserRole = async (req: Request, res: Response) => {
+  const BAD_REQUEST = 'request is not valid';
+  const UNAUTHORIZED_ID = 'id not valid';
+  const UNAUTHORIZED_TOKEN = 'token is expired';
+  const NOT_FOUND = 'no user with given id found';
+  const OK = '사용자 권한 수정 성공';
+
+  try {
+    // TODO: id -> userid 로 수정
+    const id = req.params.id;
+    const { token } = req.body;
+    const decoded = jwt.verify(token, process.env.SIGNUP_TOKEN_SECRET!) as {
+      id: string;
+      exp: number;
+    };
+    if (decoded.id !== id) throw new Error(UNAUTHORIZED_ID);
+    if (decoded.exp * 1000 < Date.now()) throw new Error(UNAUTHORIZED_TOKEN);
+
+    const result = await getRepository(User)
+      .createQueryBuilder()
+      .update()
+      .set({
+        role: UserRoleEnum.USER,
+      })
+      .where('id = :id', { id })
+      .andWhere(`role = 'GUEST'`)
+      .execute();
+    if (!result.affected) throw new Error(NOT_FOUND);
+    res.json({
+      message: OK,
+      id,
+    });
+  } catch (e) {
+    if ((e as Error).message === NOT_FOUND) {
+      res
+        .status(404)
+        .json({ message: '회원가입 실패: ' + (e as Error).message });
+    } else if ((e as Error).message === UNAUTHORIZED_ID) {
+      res.status(403).json({ message: '회원가입 실패: ' + 'id 변조됨' });
+    } else if ((e as Error).message === UNAUTHORIZED_TOKEN) {
+      res.status(403).json({ message: '회원가입 실패: ' + UNAUTHORIZED_TOKEN });
+    } else {
+      res.status(400).json({ message: '회원가입 실패: ' + BAD_REQUEST });
+    }
   }
 };
