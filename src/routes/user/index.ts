3bfa53f9fb229f24a06825473b91d32a8ffaeae3
rudy3091 <grayblack313@gmail.com>
2021-12-28 03:10:10 +0900

    Register controller for /user router
    /user 경로로 post 요청을 처리할 수 있도록 controller 함수 등록

--- src/routes/user/index.ts
+++ src/routes/user/index.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
-import helloWorld from '../hello-world';
 import idRouter from './id';
 import roleRouter from './role';
 import profileRouter from './profile';
+import { saveUser } from '../../services/User';
 
 const router = Router();
-router.post('/', helloWorld);
+router.post('/', saveUser);
 router.use('/:id', idRouter);
 router.use('/role', roleRouter);
 router.use('/profile', profileRouter);
