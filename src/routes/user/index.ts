138ca09629568626b9b04011cbb428cea53f0090
rudy3091 <grayblack313@gmail.com>
2021-12-30 17:59:32 +0900

    Refactor router structure
    /api/user/role 라우터를 /api/user/userid 아래로 이동

--- src/routes/user/index.ts
+++ src/routes/user/index.ts
@@ -1,13 +1,11 @@
 import { Router } from 'express';
 import userIdRouter from './userid';
-import roleRouter from './role';
 import profileRouter from './profile';
 import { saveUser } from '../../services/User';
 
 const router = Router();
 router.post('/', saveUser);
 router.use('/:id', userIdRouter);
-router.use('/role', roleRouter);
 router.use('/profile', profileRouter);
 
 export default router;
---
+++ src/routes/user/userid/role/index.ts
@@ -0,0 +1,7 @@
+import { Router } from 'express';
+import { changeUserRole } from '../../../../services/User';
+
+const router = Router();
+router.patch('/', changeUserRole);
+
+export default router;
