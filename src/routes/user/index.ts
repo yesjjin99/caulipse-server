de97121a6921ee320639981468f831c720186571
rudy3091 <grayblack313@gmail.com>
2021-12-30 17:49:12 +0900

    Fix POST /api/user controller
    helloworld controller를 구현한 controller로 수정

--- src/routes/user/index.ts
+++ src/routes/user/index.ts
@@ -1,12 +1,11 @@
 import { Router } from 'express';
-import helloWorld from '../hello-world';
 import userIdRouter from './userid';
 import roleRouter from './role';
 import profileRouter from './profile';
 import { saveUser } from '../../services/User';
 
 const router = Router();
-router.post('/', helloWorld);
+router.post('/', saveUser);
 router.use('/:id', userIdRouter);
 router.use('/role', roleRouter);
 router.use('/profile', profileRouter);
