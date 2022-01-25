b98d3d203f7c75e059e26d125be85fc06759139b
yesjjin99 <yesjjin99@naver.com>
2022-01-25 15:39:27 +0900

    Add swagger definitions
    comment, user Entity 스웨거 코드 추가

--- src/entity/UserEntity.ts
+++ src/entity/UserEntity.ts
@@ -56,3 +56,34 @@ export default class User {
   })
   categories!: Category[];
 }
+
+/**
+ * @swagger
+ * definitions:
+ *  User:
+ *    type: object
+ *    properties:
+ *      id:
+ *        type: string
+ *        format: uuid
+ *        description: "사용자 고유번호"
+ *      email:
+ *        type: string
+ *        description: "로그인 시 사용되는 email"
+ *      password:
+ *        type: string
+ *        description: "로그인시 사용되는 비밀번호 (암호화됨)"
+ *      isLogout:
+ *        type: boolean
+ *        description: "유저가 현재 로그아웃 상태인지 표시"
+ *      token:
+ *        type: string
+ *        description: "인증에 사용될 유저의 refresh token"
+ *      role:
+ *        type: string
+ *        enum:
+ *        - "GUEST"
+ *        - "USER"
+ *        - "ADMIN"
+ *        description: "사용자의 권한을 표시"
+ */
