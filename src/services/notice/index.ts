146119ae40355520e06e15938fe44fa414e6c49f
yesjjin99 <yesjjin99@naver.com>
2022-02-23 04:27:53 +0900

    Add notice API
    공지사항 생성, 상세정보 조회, 삭제 엔드포인트 구현

--- src/services/notice/index.ts
+++ src/services/notice/index.ts
@@ -1,6 +1,38 @@
+import { randomUUID } from 'crypto';
 import { getRepository } from 'typeorm';
 import Notice from '../../entity/NoticeEntity';
+import User from '../../entity/UserEntity';
 
 export const findAllNotice = async () => {
   await getRepository(Notice).createQueryBuilder().select().execute();
+};
+
+export const findNoticeById = async (id: string) => {
+  return await getRepository(Notice)
+    .createQueryBuilder('notice')
+    .leftJoinAndSelect('notice.hostId', 'user')
+    .where('notice.id = :id', { id })
+    .getOne();
+};
+
+export const createNotice = async (
+  title: string,
+  about: string,
+  user: User
+) => {
+  const noticeId = randomUUID();
+  const notice = new Notice();
+  notice.id = noticeId;
+  notice.title = title;
+  notice.about = about;
+  notice.createdAt = new Date();
+  notice.views = 0;
+  notice.hostId = user;
+
+  await getRepository(Notice).save(notice);
+  return noticeId;
+};
+
+export const deleteNotice = async (notice: Notice) => {
+  return await getRepository(Notice).remove(notice);
 };
