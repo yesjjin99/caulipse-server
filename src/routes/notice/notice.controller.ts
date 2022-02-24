cb5574c2924382825030fe665fffe0f319851a63
rudy3091 <grayblack313@gmail.com>
2022-02-24 09:37:59 +0900

    Add swagger doc for PATCH /notice/:noticeid

--- src/routes/notice/notice.controller.ts
+++ src/routes/notice/notice.controller.ts
@@ -50,3 +50,79 @@ export default {
     }
   },
 };
+
+/**
+ * @swagger
+ * /api/notice/{noticeid}:
+ *   patch:
+ *     summary: "공지사항 정보 업데이트"
+ *     description: "각 공지사항의 정보를 업데이트하기 위한 엔드포인트입니다"
+ *     tags:
+ *     - "notice"
+ *     consumes:
+ *     - "application/json"
+ *     produces:
+ *     - "application/json"
+ *     parameters:
+ *     - name: "noticeid"
+ *       in: "path"
+ *       description: "수정할 공지사항 id"
+ *       required: true
+ *       type: string
+ *       format: uuid
+ *     - in: "body"
+ *       name: "notice_body"
+ *       description: "수정할 공지사항 정보 객체"
+ *       required: true
+ *       schema:
+ *         type: object
+ *         allOf:
+ *           - type: object
+ *             properties:
+ *               title:
+ *                 type: string
+ *               noticeAbout:
+ *                 type: string
+ *
+ *     responses:
+ *       200:
+ *         description: "올바른 요청"
+ *         schema:
+ *           type: object
+ *           properties:
+ *             message:
+ *               type: string
+ *               example: "공지사항 정보 업데이트 성공"
+ *       400:
+ *         description: "요청값이 유효하지 않은 경우입니다"
+ *         schema:
+ *           type: object
+ *           properties:
+ *             message:
+ *               type: string
+ *               example: "request is not valid"
+ *       401:
+ *         description: "로그인이 되어있지 않은 경우"
+ *         schema:
+ *           type: object
+ *           properties:
+ *             message:
+ *               type: string
+ *               example: "로그인 필요"
+ *       403:
+ *         description: "로그인된 사용자가 어드민 계정이 아닌 경우"
+ *         schema:
+ *           type: object
+ *           properties:
+ *             message:
+ *               type: string
+ *               example: "권한이 없어 승인 불가능"
+ *       404:
+ *         description: "전달한 noticeid가 데이터베이스에 없는 경우입니다"
+ *         schema:
+ *           type: object
+ *           properties:
+ *             message:
+ *               type: string
+ *               example: "일치하는 noticeid가 없음"
+ */
