bf41f426ab9ed3ccc86d58445e6166611a9fe7f2
rudy3091 <grayblack313@gmail.com>
2022-02-27 12:17:13 +0900

    Add https://localhost:3000 for cors origin

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ const swaggerSpec = swaggerJSDoc(swaggerOption);
 const app = express();
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'https://localhost:3000'],
     credentials: true,
   })
 );
