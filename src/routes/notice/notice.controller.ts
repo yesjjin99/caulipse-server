2b112701cff912eda1a563f23f3fb051a3d434be
rudy3091 <grayblack313@gmail.com>
2022-03-01 23:30:13 +0900

    Add notice api impl WIP

--- src/routes/notice/notice.controller.ts
+++ src/routes/notice/notice.controller.ts
@@ -12,15 +12,24 @@ import { UserRoleEnum } from '../../entity/UserEntity';
 export default {
   async findAllNotice(req: Request, res: Response) {
     try {
-      const rowNum = req.query.row_num || 12;
-      const cursor = req.query.cursor || ''; // FIXME: cursor 디폴트값 설정
+      const amount = req.query.amount || 12;
+      const offset = req.query.offset || 0; // FIXME: cursor 디폴트값 설정
 
       const result = await findAllNotice({
-        amount: rowNum as number,
-        cursor: cursor as string,
+        amount: +amount,
+        offset: +offset,
       });
-      res.json(result);
+      const response = result.map((item: Record<string, string | Date>) => ({
+        id: item.Notice_ID,
+        title: item.Notice_TITLE,
+        about: item.Notice_ABOUT,
+        createdAt: item.Notice_CREATED_AT,
+        views: item.Notice_VIEWS,
+        hostId: item.Notice_HOST_ID,
+      }));
+      res.json(response);
     } catch (e) {
+      console.log((e as Error).message);
       res.status(500).json({ message: '오류 발생' });
     }
   },
