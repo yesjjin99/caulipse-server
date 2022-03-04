ddd3db1c5f49f672629b7d9914b95119954a2e91
rudy3091 <grayblack313@gmail.com>
2022-03-04 22:32:07 +0900

    Replace magic number with variable
    시간을 의미하는 상수값을 변수화해서 가독성 향상

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -27,8 +27,9 @@ export const refresh = async (req: Request, res: Response) => {
 
     const newAccessToken = generateToken({ id: decoded.id });
 
+    const hour = 3600 * 1000;
     res.cookie('accessToken', newAccessToken, {
-      expires: new Date(Date.now() + 1000 * 60 * 60 * 3),
+      expires: new Date(Date.now() + 3 * hour),
       domain: 'cau.rudy3091.com',
       sameSite: 'none',
       secure: true,
