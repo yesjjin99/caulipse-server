c0ca529c52d1dc4f0cf638931555d9504aba1675
Jeyoung-Park <jason9380@naver.com>
2022-03-08 21:23:25 +0900

    fix: category 서비스 로직 삭제

--- src/services/category/index.ts
+++
@@ -1,9 +0,0 @@
-import { getRepository } from 'typeorm';
-import User from '../../entity/UserEntity';
-
-export const postInterestCategory = async (user: User, category: number) => {
-  await getRepository(User).save({
-    ...user,
-    categories: [category],
-  });
-};
