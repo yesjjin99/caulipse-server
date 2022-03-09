815ab70d195b16c2f6ae385225705590d7146902
rudy3091 <grayblack313@gmail.com>
2022-03-09 22:08:31 +0900

    Add email html template
    이메일의 몸체에 사용될 html 템플릿 추가
    아직 우리 서비스로 이동시킬 url을 지정하지 않음

--- src/services/mail/html.ts
+++ src/services/mail/html.ts
@@ -1,3 +1,102 @@
-export const html = () => `<body>
-  <h1>caulipse 회원가입</h1>
-</body>`;
+const _createElement = ({
+  tagName,
+  className,
+  styles,
+  children,
+  attr,
+}: {
+  tagName: string;
+  className: string;
+  styles: [string, string][];
+  children?: string[];
+  attr?: [string, string][];
+}) => `
+<${tagName}
+  class="${className}"
+  style="${styles.map(([key, value]) => `${key}: ${value};`).join('')}"
+  ${attr?.map(([key, value]) => `${key}=${value} `).join('')}
+>
+  ${children?.join('')}
+</${tagName}>`;
+
+const _greeting = () =>
+  _createElement({
+    tagName: 'div',
+    className: 'greeting',
+    styles: [
+      ['font-size', '18px'],
+      ['font-weight', '900'],
+    ],
+    children: ['<h2>~~님 환영합니다!</h2>'],
+  });
+
+const _instruction = () =>
+  _createElement({
+    tagName: 'div',
+    className: 'instruction',
+    styles: [
+      ['margin', '8px 0 64px 0'],
+      ['font-size', '16px'],
+    ],
+    children: ['<span>인증 버튼을 눌러 회원가입을 완료하세요</span>'],
+  });
+
+const _button = () =>
+  _createElement({
+    tagName: 'button',
+    className: 'button',
+    styles: [
+      ['border', 'none'],
+      ['border-radius', '8px'],
+      ['background-color', '#7393ff'],
+      ['padding', '12px 18px'],
+      ['font-size', '16px'],
+      ['font-weight', '700'],
+      ['color', '#fff'],
+      ['cursor', 'pointer'],
+    ],
+    children: ['<span>중앙대 인증</span>'],
+    attr: [['role', 'button']],
+  });
+
+const _link = (id: string, token: string) =>
+  _createElement({
+    tagName: 'a',
+    className: 'button',
+    styles: [['text-decoration', 'none']],
+    children: [_button()],
+    attr: [
+      [
+        'href',
+        `https://github.com/caulipse/caulipse-server?id=${id}&token=${token}`,
+      ], // TODO: 우리 서비스 주소로 리다이렉트
+      ['target', '_blank'],
+    ],
+  });
+
+const _container = (id: string, token: string) =>
+  _createElement({
+    tagName: 'div',
+    className: 'container',
+    styles: [
+      ['max-width', '500px'],
+      ['margin', '0 auto'],
+      ['padding', '32px'],
+      ['background-color', '#f8f8f8'],
+      ['text-align', 'center'],
+      ['border-radius', '8px'],
+    ],
+    children: [_greeting(), _instruction(), _link(id, token)],
+  });
+
+export const html = (id: string, token: string) =>
+  _createElement({
+    tagName: 'div',
+    className: 'body',
+    styles: [
+      ['width', '100%'],
+      ['padding', '0'],
+      ['margin', '0'],
+    ],
+    children: [_container(id, token)],
+  });
