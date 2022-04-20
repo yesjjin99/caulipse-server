c4f464f3d426e0c7adb4bac5d6d69a7ec6e4896c
rudy3091 <grayblack313@gmail.com>
2022-04-21 00:03:59 +0900

    Fix cors issue

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,11 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(
   cors({
-    origin: ['http://localhost:3000', 'https://localhost:3000'],
+    origin: [
+      'http://localhost:3000',
+      'https://localhost:3000',
+      'https://caustudy.com',
+    ],
     credentials: true,
   })
 );
